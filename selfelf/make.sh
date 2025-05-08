0f87da70b059c7617c489336f2d78d5479a833f2
iwl-lyam <develop331@itwithlyam.co.uk>
2025-05-08 12:12:54 +0000

    hello world program

--- selfelf/make.sh
+++ selfelf/make.sh
@@ -3,4 +3,10 @@ for f in *.dmp ; do
     a=`basename $f .dmp`
     cut -d'#' -f1 <$f | xxd -p -r >$a
     chmod +x $a
+
+    readelf -a $a
+    echo ""
+    echo "RUNNING PROGRAM"
+    echo ""
+    ./$a
 done
